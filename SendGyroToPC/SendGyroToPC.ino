3c94975b7ea589c88daac5ff52cb1aecd3a76621
Work <banana99ou@gmail.com>
2025-03-07 15:47:44 +0900

    change to Serial

--- SendGyroToPC/SendGyroToPC.ino
+++ SendGyroToPC/SendGyroToPC.ino
@@ -1,60 +1,57 @@
-#include <WiFi.h>
 #include <Wire.h>
 #include <MPU6050_tockn.h>
 
+// Create a union to easily convert float to 4 bytes
+typedef union {
+  float number;
+  uint8_t bytes[4];
+} FLOATUNION_t;
+
 MPU6050 mpu(Wire);
 
-WiFiServer server(8888);
-
-const char* ssid     = "FMCL";
-const char* password = "66955144";
-
-int counter = 0;
+// Array of 6 union objects for acc and gyro data
+FLOATUNION_t sensorData[6];
 
 void setup() {
-  Serial.begin(115200);
-
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nWiFi connected");
-
-  Serial.print("Camera Ready! Use 'http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("' to connect");
-
-  server.begin();  // Start listening on port 8888
-  Serial.println("Server started, waiting for client...");
-
-  Wire.begin(15, 14, 100000);
+  Serial.begin(115200);  // Must match baud rate in Simulink
+  Wire.begin(15, 14, 100000);  // SDA, SCL, freq
   mpu.begin();
   mpu.calcGyroOffsets(true);
   Serial.println("MPU6050 initialized and calibrated.");
 }
 
 void loop() {
-    WiFiClient client = server.available();
-    if (client) {
-        while(client.connected()){
-            // Update the MPU6050 data
-            mpu.update();
-            float dataOut [6];
-            dataOut[0] = mpu.getAccX();
-            dataOut[1] = mpu.getAccY();
-            dataOut[2] = mpu.getAccZ();
-            dataOut[3] = mpu.getGyroX();
-            dataOut[4] = mpu.getGyroY();
-            dataOut[5] = mpu.getGyroZ();
-            //dataOut[6] = mpu.getTemp();
+  // Update the MPU6050 data
+  mpu.update();
 
+  // Read the sensor values
+  float accX = mpu.getAccX();
+  float accY = mpu.getAccY();
+  float accZ = mpu.getAccZ();
+  float gyroX = mpu.getGyroX();
+  float gyroY = mpu.getGyroY();
+  float gyroZ = mpu.getGyroZ();
 
-            Serial.println("sending");
-            Serial.println(String(dataOut[0]));
-            // Once connected, send data
-            client.write(reinterpret_cast<uint8_t*>(dataOut), sizeof(dataOut));
-            delay(20);
-        }
+  // Store them into our union array
+  sensorData[0].number = accX;
+  sensorData[1].number = accY;
+  sensorData[2].number = accZ;
+  sensorData[3].number = gyroX;
+  sensorData[4].number = gyroY;
+  sensorData[5].number = gyroZ;
+
+  // Print header (1 byte)
+  Serial.write('A');
+
+  // Print 6 floats (6 * 4 = 24 bytes)
+  for (int i = 0; i < 6; i++) {
+    for (int b = 0; b < 4; b++) {
+      Serial.write(sensorData[i].bytes[b]);
     }
+  }
+
+  // Print terminator
+  Serial.print('\n');
+
+  delay(50);  // Adjust to match the Simulink Serial Receive block sampling rate
 }
