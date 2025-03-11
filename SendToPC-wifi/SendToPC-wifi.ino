db98536b133e1df8142f588b3b42559d63b232a2
Work <banana99ou@gmail.com>
2025-03-11 17:40:27 +0900

    varified simple 2 channel recieve

--- SendToPC-wifi/SendToPC-wifi.ino
+++ SendToPC-wifi/SendToPC-wifi.ino
@@ -1,35 +1,58 @@
+typedef union{
+  float number;
+  uint8_t bytes[4];
+} FLOATUNION_t;
+
+FLOATUNION_t myValue;
+
 #include <WiFi.h>
 WiFiServer server(8888);
 
-const char* ssid     = "SK_7458_2.4G";
-const char* password = "HYV21@0527";
+const char* SSID     = "FMCL";
+const char* PASSWORD = "66955144";
 
-int counter = 0;
+int counter1 = 0;
+float counter2 = 0;
 
 void setup() {
   Serial.begin(115200);
 
-  WiFi.begin(ssid, password);
+  WiFi.begin(SSID, PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("\nWiFi connected");
-
-  server.begin();  // Start listening on port 8888
-  Serial.println("Server started, waiting for client...");
+  Serial.println(" connected!");
+  server.begin();
+  Serial.print("Camera Stream Ready! Go to: http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/stream");
 }
 
 void loop() {
   WiFiClient client = server.available();
   if (client) {
     while(client.connected()){
+      Serial.write('A'); 
       Serial.print("sending");
-      Serial.println(counter);
-      // Once connected, send data
-      client.println(counter);
-      counter++;
-      delay(1000);
+      Serial.print(counter1);
+      Serial.print(", ");
+      Serial.println(counter2);
+
+      myValue.number = counter1;
+      for (int i=0; i<4; i++){
+        client.write(myValue.bytes[i]); 
+      }
+      
+      myValue.number = counter2;
+      for (int i=0; i<4; i++){
+        client.write(myValue.bytes[i]); 
+      }
+      
+      // Use the same delay in the Serial Receive block
+      counter1++;
+      counter2 = 4 + counter2;
+      delay(50);
     }
   }
 }
