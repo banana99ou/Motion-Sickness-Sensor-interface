7fae26ca7874eb788a71b81d792d459f072efcea
banana99ou <banana99ou@gmail.com>
2025-05-21 13:44:42 +0900

    make wifi configurable

--- test/test.ino
+++ test/test.ino
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84d073d6a82e8bd23911b0c8947519c4437e05226dee82cf2abba03befe9167d
-size 9475
+oid sha256:c1b9b5c029681fed9b109d10f187d8db1ea7301044f6156db2bd05a924197539
+size 9978
